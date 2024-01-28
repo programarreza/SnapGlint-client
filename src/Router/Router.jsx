9018b372deeeabadad7a8a16f270946c866e1cdd
programarreza <programarreza@gmail.com>
2024-01-28 11:56:33 +0600

    added to login page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,23 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Register from "../Pages/Register/Register";
-
+import Login from "../Pages/Login/Login";
 
 const Router = createBrowserRouter([
-	{
-		path: "/",
-		element: <MainLayout/>,
-		children: [
-			{
-				path: "/",
-				element: <Home/>
-			},
-			{
-				path: "register",
-				element: <Register/>
-			}
-		]
-	}
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 export default Router;
