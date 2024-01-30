95fb14fbbaaaf489705269b377fc8f5df7a308a4
programarreza <programarreza@gmail.com>
2024-01-30 06:23:57 +0600

    deploy and reame file update

--- src/hooks/useAxiosLocal.jsx
+++ src/hooks/useAxiosLocal.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosLocal = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://snapglint.onrender.com",
 });
 const useAxiosLocal = () => {
   return axiosLocal;
